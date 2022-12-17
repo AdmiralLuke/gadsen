568310793a3db47545b35742dfeca83098eb0906
Cornelius Zenker <cornelius.o.zenker@gmail.com>
2022-12-17 21:19:05 +0100

    #225 Projectiles are rendered now

--- core/src/com/gats/animation/Projectiles.java
+++ core/src/com/gats/animation/Projectiles.java
@@ -2,7 +2,9 @@ package com.gats.animation;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.gats.animation.entity.AnimatedEntity;
 import com.gats.animation.entity.Entity;
 import com.gats.animation.entity.SpriteEntity;
@@ -14,13 +16,20 @@ public class Projectiles {
     protected static TextureAtlas projectileAtlas;
 
     protected static Entity summon(ProjectileAction.ProjectileType type){
+        Array<TextureAtlas.AtlasRegion> texture;
         switch (type){
             case COOKIE:
-                return new AnimatedEntity(new Animation<>(1/8f, projectileAtlas.findRegions("tile/#161_cookieTumblingCropped")), new Vector2(1,1));
+                texture = projectileAtlas.findRegions("tile/#161_cookieTumblingCropped");
+                break;
             case CANDY_CANE:
-                return new AnimatedEntity(new Animation<>(1/8f, projectileAtlas.findRegions("tile/sugarcaneProjectileFront")), new Vector2(1,1));
+                texture = projectileAtlas.findRegions("tile/sugarcaneProjectileFront");
+                break;
+            default:
+                throw new RuntimeException("Type " + type + " is not Supported!");
 
         }
-        throw new RuntimeException("Type " + type + " is not Supported!");
+        Animation<TextureRegion> animation = new Animation<>(1/8f, texture);
+        animation.setPlayMode(Animation.PlayMode.LOOP);
+        return new AnimatedEntity(animation, new Vector2(texture.get(0).getRegionWidth(), texture.get(0).getRegionHeight()));
     }
 }
