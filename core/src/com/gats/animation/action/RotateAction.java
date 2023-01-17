6b9448bf26d0174e0998688fb63faec0807335cf
Nicolas <nicolas.kautt@st.ovgu.de>
2023-01-17 15:38:12 +0100

    #251 Winkel von SpriteEntity zu Entity bewegt. Dazu RotateAction angepasst

--- core/src/com/gats/animation/action/RotateAction.java
+++ core/src/com/gats/animation/action/RotateAction.java
@@ -2,17 +2,16 @@ package com.gats.animation.action;
 
 import com.badlogic.gdx.math.Vector2;
 import com.gats.animation.entity.Entity;
-import com.gats.animation.entity.SpriteEntity;
 
 /**
- * Simple Animator Action that sets the angle of a SpriteEntity.
+ * Simple Animator Action that sets the angle of a Entity.
  */
 public class RotateAction extends Action{
 
-    private SpriteEntity target;
+    private Entity target;
     private Vector2 angle;
 
-    public RotateAction(float start,SpriteEntity target, Vector2 angle) {
+    public RotateAction(float start,Entity target, Vector2 angle) {
         super(start);
         this.target = target;
         this.angle = angle;
