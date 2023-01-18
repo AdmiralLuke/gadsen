18cd7fd164f5e6ffb2518126623f06559c156161
Nicolas <nicolas.kautt@st.ovgu.de>
2023-01-18 14:26:29 +0100

    #244 Put some Spagetthi into MapRetriever to also load maps from the maps/ folder in the jars directory

--- core/src/com/gats/ui/MapRetriever.java
+++ core/src/com/gats/ui/MapRetriever.java
@@ -1,5 +1,4 @@
 package com.gats.ui;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
 import java.io.File;
@@ -9,33 +8,50 @@ public class MapRetriever implements FilenameFilter {
     static FileHandle dirHandler;
     final String defaultMapFiletype = ".json";
     String filetype;
-    String mapDirectory =  "core/resources/maps";
+
+    /**
+     * For dev use in the Project/Ide.
+      */
+    String internalMapDirectory =  "core/resources/maps";
+    /**
+    /**
+     * Path relative to the jar after building
+     */
+    String externalMapDirectory = "maps";
 
     String[] hardCodeMapNames = new String[]{"christmasMap","map1","MapSafeGround","testingMap"};
 
 
     /**Creates a {@link MapRetriever}, wich is responsible for providing the selectable Maps
-     * found in {@link MapRetriever#mapDirectory}.
-     * Currently, does not look into subdirectories.
-     */
-    MapRetriever(){
-       new MapRetriever(mapDirectory);
-    }
-
-    /**Creates a {@link MapRetriever}, wich is responsible for providing the selectable Maps
      * found in a directory.
      * Currently, does not look into subdirectories.
-     * @param newDirectory Changes the {@link MapRetriever#mapDirectory}, where it will look for maps.
+     * assigns it the directory that can be found
+     * first checks if the internal directory is available
+     * then looks for for the external dir
      */
-     MapRetriever(String newDirectory) {
 
-        dirHandler = null;//Gdx.files.internal(newDirectory);
-        if(dirHandler!=null) {
-            if (!dirHandler.isDirectory()) {
-                throw new RuntimeException("Provided Path for MapHandler is not a valid directory!");
-            }
+    MapRetriever() {
+        FileHandle directory = determineMapDirectory(internalMapDirectory);
+        if (directory!=null){
+            dirHandler = directory;
         }
+        else {
+            directory = determineMapDirectory(externalMapDirectory);
+            dirHandler = directory;
+        }
+    }
 
+    /**
+     * checks whether the given directory exists
+     * @param directoryPath Directory to check
+     * @return directory if it exists, otherwise returns null
+     */
+    public FileHandle determineMapDirectory(String directoryPath){
+        FileHandle dir = new FileHandle(directoryPath);
+        if(dir.isDirectory()){
+            return dir;
+        }
+        return null;
     }
 
     /**
@@ -50,36 +66,37 @@ public class MapRetriever implements FilenameFilter {
     }
     /**
      * Lists all Map files, with the default File Extension,
-     * in {@link MapRetriever#mapDirectory}.
+     * in {@link MapRetriever#internalMapDirectory}.
      * @return String Array with map Names located in mapDirectory
      */
     public String[] listMaps(){
-        this.filetype = defaultMapFiletype;
-        String[] mapNames;
-        if(dirHandler!=null) {
-           mapNames = fileHandleToStringArray(dirHandler.list(this));
-        }
-        else{
-            mapNames = this.hardCodeMapNames;
-        }
-        removeExtension(mapNames);
-        //does not work yet
-        return mapNames;
+        return listMaps(defaultMapFiletype);
     }
 
 
 
     /**
      * Lists all Maps files, with the provided File Extension,
-     * in {@link MapRetriever#mapDirectory}.
+     * in {@link MapRetriever#internalMapDirectory}.
      * @param fileExtension Map File Extension.
      * @return String Array with map Names located in mapDirectory
      */
     public String[] listMaps(String fileExtension){
         this.filetype = fileExtension;
-        String[] maps= fileHandleToStringArray(dirHandler.list(this));
-        removeExtension(maps);
-        return maps;
+         String[] mapNames = new String[0];
+
+        if(dirHandler!=null) {
+            mapNames = fileHandleToStringArray(dirHandler.list(this));
+            removeExtension(mapNames);
+        }
+        if(dirHandler==null || !dirHandler.toString().equals(internalMapDirectory)) {
+            /**
+             * If the directory is not the internal, then hardcoded maps should be added to the directory.
+             * Otherwise they would be redundandly listed.
+             */
+            mapNames = appendHardcodeMaps(mapNames);
+        }
+        return mapNames;
     }
 
     public String[] fileHandleToStringArray(FileHandle[] handles){
@@ -90,6 +107,19 @@ public class MapRetriever implements FilenameFilter {
             }
         return fileHandleNames;
 
+    }
+    public String[] appendHardcodeMaps(String[] maps){
+        String[] newMaps = new String[maps.length+hardCodeMapNames.length];
+        int counter = 0;
+        for (String map:hardCodeMapNames) {
+            newMaps[counter] = map;
+            counter++;
+        }
+        for (String map:maps) {
+            newMaps[counter] = map;
+            counter++;
+        }
+        return newMaps;
     }
 
     /** Checks whether a given file contains the given extension .
