08ae31bda959785fb679aea97e2b03b6ad77ec07
Cornelius Zenker <cornelius.o.zenker@gmail.com>
2023-03-21 19:23:52 +0100

    #318 #319 DebugPointAction will now be rendered on screen; isCross attribute can be set to true to render a small cross instead

--- core/src/com/gats/simulation/action/DebugPointAction.java
+++ core/src/com/gats/simulation/action/DebugPointAction.java
@@ -8,21 +8,25 @@ public class DebugPointAction extends Action{
     private final Vector2 pos;
     private final Color color;
     private final float duration;
+    private boolean isCross;
 
     /**
      * Creates an Action that will place a point on screen for debugging-purposes.
-     * Will be replaced by ParticleAction in the future
+     * Will be replaced by ParticleAction in the future.
+     * The crossOption can be used to render a small 3px cross instead for better visibility.
      *
      * @param delay     non-negative time-based offset to its parent in seconds
      * @param pos       position the point should appear at
      * @param color     the color of the point
      * @param duration  how long the point lasts in seconds
+     * @param isCross   true if the indicator should be a small cross instead
      */
-    public DebugPointAction(float delay, Vector2 pos, Color color, float duration) {
+    public DebugPointAction(float delay, Vector2 pos, Color color, float duration, boolean isCross) {
         super(delay);
         this.pos = pos;
         this.color = color;
         this.duration = duration;
+        this.isCross = isCross;
     }
 
     /**
@@ -43,5 +47,12 @@ public class DebugPointAction extends Action{
      */
     public float getDuration() {
         return duration;
+    }
+
+    /**
+     * @return true if the indicator should be a small cross instead
+     */
+    public boolean isCross() {
+        return isCross;
     }
 }
