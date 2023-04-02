11d34eb77d4fc7a36d34598bca9772025ca9a395
Nicolas <nicolas.kautt@st.ovgu.de>
2023-04-03 01:24:43 +0200

    #376 Tileset mit den neuen Boxen erstellt, um diese in die Map erstellung  mit einzubinden.
    Für jede Sprite welche noch nicht im Projekt vorhanden ist (durch noch fehlenden Merge),
    gibt es eine temporäre Datei im tiledProject

---
+++ assets/res/maps/tiledProject/tileset/TilesetNewBoxes.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="TilesetWithNewBoxes" tilewidth="60" tileheight="60" tilecount="7" columns="0" objectalignment="center" tilerendersize="grid" fillmode="preserve-aspect-fit">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="Beschreibung" value="Ankerbox"/>
+  </properties>
+  <image width="16" height="16" source="../../../texture/tile/anchor/16x_anchor02.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Beschreibung" value="Normale Box"/>
+  </properties>
+  <image width="16" height="16" source="../../../texture/tile/box/16x_box01DarkEdges.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Beschreibung" value="Spawnpunkt"/>
+  </properties>
+  <image width="60" height="60" source="../../../texture/ui/cat_lowRes.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Beschreibung" value="Medibox"/>
+  </properties>
+  <image source="../assets/healthBoxRC.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Beschreibung" value="Waffenbox-Anker"/>
+  </properties>
+  <image source="../assets/weaponBoxAnchor.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Beschreibung" value="Waffenbox-Normal"/>
+  </properties>
+  <image source="../assets/weaponBoxChest.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Beschreibung" value="Schleimbox"/>
+  </properties>
+  <image source="../assets/slimeBox.png"/>
+ </tile>
+</tileset>
