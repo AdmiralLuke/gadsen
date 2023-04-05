cbe7f057653338ca016d7724ee870498a130e1fc
Nicolas <nicolas.kautt@st.ovgu.de>
2023-04-05 03:03:43 +0200

    #302 comment in turn timer

--- core/src/com/gats/ui/hud/TurnTimer.java
+++ core/src/com/gats/ui/hud/TurnTimer.java
@@ -7,17 +7,23 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
+/**
+ * Class representing and drawing the TimerSprite and the remaining time.
+ */
 public class TurnTimer extends HorizontalGroup {
 
 	Image timerImage;
 	Label timeDisplay;
 
 	public TurnTimer(TextureRegion timer, Skin labelSkin){
+
 		this.timerImage = new Image(timer);
 		//Todo, change label to ColoredLabel from other branch
 		timeDisplay = new Label("",labelSkin);
 		timeDisplay.setColor(Color.GREEN);
 
+
+		//add both to the horizontal group for drawing them next to each other
 		addActor(timeDisplay);
 
 		space(5);
