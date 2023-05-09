1ec2cd1d081272f58012dd285f9f7f346e0be248
Cornelius Zenker <cornelius.o.zenker@gmail.com>
2023-05-09 21:13:34 +0200

    #475 #476 implemented implemented campaign week 2

--- core/src/com/gats/simulation/campaign/CampaignResources.java
+++ core/src/com/gats/simulation/campaign/CampaignResources.java
@@ -9,10 +9,15 @@ public class CampaignResources {
     public static ArrayList<Class<? extends Player>> getEnemies(String map){
         ArrayList<Class<? extends Player>> enemies = new ArrayList<>();
         switch (map){
-            case "level1":
+            case "level1_1":
+            case "level1_2":
+            case "level1_3":
                 enemies.add(Level1Bot.class);
                 break;
-            case "level2":
+            case "level2_1":
+            case "level2_2":
+            case "level2_3":
+                enemies.add(Level2Bot.class);
                 break;
             case "level3":
                 break;
@@ -27,13 +32,18 @@ public class CampaignResources {
     public static ArrayList<int[]> getWeaponCounts(String map){
         ArrayList<int[]> inventories = new ArrayList<>();
         switch (map){
-            case "level1":
+            case "level1_1":
                 inventories.add(new int[]{0, 0, 0, 0, 0, 0});
                 inventories.add(new int[]{0, 1, 0, 0, 0, 0});
                 break;
-            case "level2":
-                inventories.add(new int[]{0, 0, 0, 0, 0, 0});
-                inventories.add(new int[]{0, 0, 0, 0, 0, 0});
+            case "level1_2":
+            case "level1_3":
+
+            case "level2_1":
+            case "level2_2":
+            case "level2_3":
+                inventories.add(new int[]{0, 99, 0, 0, 0, 0});
+                inventories.add(new int[]{0, 99, 0, 0, 0, 0});
                 break;
             case "level3":
                 inventories.add(new int[]{0, 0, 0, 0, 0, 0});
@@ -52,21 +62,26 @@ public class CampaignResources {
     public static ArrayList<int[]> getHealth(String map){
         ArrayList<int[]> helth = new ArrayList<>();
         switch (map){
-            case "level1":
+            case "level1_1":
                 helth.add(new int[]{5});
                 helth.add(new int[]{100});
                 break;
-            case "level2":
-                helth.add(new int[]{0});
-                helth.add(new int[]{0});
+            case "level1_2":
+                helth.add(new int[]{100});
+                helth.add(new int[]{15});
                 break;
-            case "level3":
-                helth.add(new int[]{0});
-                helth.add(new int[]{0});
+            case "level1_3":
+                helth.add(new int[]{100});
+                helth.add(new int[]{80});
                 break;
-            case "level4":
-                helth.add(new int[]{0});
-                helth.add(new int[]{0});
+            case "level2_1":
+                helth.add(new int[]{100});
+                helth.add(new int[]{100});
+                break;
+            case "level2_2":
+            case "level2_3":
+                helth.add(new int[]{80});
+                helth.add(new int[]{100});
                 break;
             case "level5":
                 break;
--- core/src/com/gats/simulation/campaign/Level2Bot.java
+++ core/src/com/gats/simulation/campaign/Level2Bot.java
@@ -5,14 +5,14 @@ import com.gats.manager.Controller;
 import com.gats.simulation.GameState;
 import com.gats.simulation.WeaponType;
 
-public class Level1Bot extends StaticBot {
+public class Level2Bot extends StaticBot {
 
     @Override
     protected void executeTurn(GameState state, Controller controller) {
         super.executeTurn(state, controller);
         switch (turnCount){
             case 0:
-                controller.aim(new Vector2(-1, 0), 1);
+                controller.aim(new Vector2(-1, 0.1f), 1);
                 controller.selectWeapon(WeaponType.WATER_PISTOL);
             default:
                 controller.shoot();
