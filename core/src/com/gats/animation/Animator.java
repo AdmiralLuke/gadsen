1e2034176a6d07cc7196178e6a1443e985587c68
Cornelius Zenker <cornelius.o.zenker@gmail.com>
2023-05-10 23:08:18 +0200

    #485 fixed a bug where the game would crash on draw

--- core/src/com/gats/animation/Animator.java
+++ core/src/com/gats/animation/Animator.java
@@ -261,11 +261,11 @@ public class Animator implements Screen, AnimationLogProcessor {
             return new ExpandedAction(summonProjectile, destroyProjectile);
         }
 
-        private static ExpandedAction addParticle(ParticleEffectPool effect, Vector2 pos, float duration, Animator animator){
+        private static ExpandedAction addParticle(ParticleEffectPool effect, Vector2 pos, float duration, Animator animator) {
             DestroyAction<ParticleEntity> destroyParticle = new DestroyAction<ParticleEntity>(duration, null, null, (entity) -> {
-            animator.root.remove(entity);
-            entity.free();
-        });
+                animator.root.remove(entity);
+                entity.free();
+            });
 
             SummonAction<ParticleEntity> summonParticle = new SummonAction<ParticleEntity>(0, destroyParticle::setTarget, () -> {
                 ParticleEntity particleEntity = ParticleEntity.getParticleEntity(effect);
@@ -362,7 +362,7 @@ public class Animator implements Screen, AnimationLogProcessor {
             CharacterSelectAction characterSelectAction = new CharacterSelectAction(startAction.getDelay(), target, animator::setActiveGameCharacter);
 
             //ui Action
-            MessageUiTurnStartAction indicateTurnStartAction = new MessageUiTurnStartAction(0, animator.uiMessenger, animator.state.getCharacterFromTeams(startAction.getTeam(), startAction.getCharacter()),target);
+            MessageUiTurnStartAction indicateTurnStartAction = new MessageUiTurnStartAction(0, animator.uiMessenger, animator.state.getCharacterFromTeams(startAction.getTeam(), startAction.getCharacter()), target);
 
             characterSelectAction.setChildren(new Action[]{indicateTurnStartAction});
             return new ExpandedAction(characterSelectAction, indicateTurnStartAction);
@@ -436,12 +436,13 @@ public class Animator implements Screen, AnimationLogProcessor {
                 SetAnimationAction resetAnimationAction = new SetAnimationAction(GameCharacter.getAnimationDuration(GameCharacterAnimationType.ANIMATION_TYPE_HIT), target, GameCharacterAnimationType.ANIMATION_TYPE_IDLE);
                 hitAnimation.setChildren(new Action[]{summonParticle, resetAnimationAction});
             }
-            UpdateHealthBarAction updateHealthBarAction  = new UpdateHealthBarAction(0,hitAction.getHealthAft(),target.getHealthbar());
+            UpdateHealthBarAction updateHealthBarAction = new UpdateHealthBarAction(0, hitAction.getHealthAft(), target.getHealthbar());
             hitAnimation.addChild(updateHealthBarAction);
-            lastAction=updateHealthBarAction;
+            lastAction = updateHealthBarAction;
 
             return new ExpandedAction(hitAnimation, lastAction);
         }
+
         private static ExpandedAction convertCharacterHealAction(com.gats.simulation.action.Action action, Animator animator) {
             CharacterHealAction hitAction = (CharacterHealAction) action;
             GameCharacter target = animator.teams[hitAction.getTeam()][hitAction.getCharacter()];
@@ -461,7 +462,7 @@ public class Animator implements Screen, AnimationLogProcessor {
                 return particleEntity;
             });
             summonParticle.setChildren(new Action[]{destroyParticle});*/
-            UpdateHealthBarAction updateHealthBarAction  = new UpdateHealthBarAction(0,hitAction.getHealthAft(),target.getHealthbar());
+            UpdateHealthBarAction updateHealthBarAction = new UpdateHealthBarAction(0, hitAction.getHealthAft(), target.getHealthbar());
             //healAnimation.addChild(updateHealthBarAction);
 
             return new ExpandedAction(updateHealthBarAction);
@@ -469,27 +470,24 @@ public class Animator implements Screen, AnimationLogProcessor {
 
         private static ExpandedAction convertGameOverAction(com.gats.simulation.action.Action action, Animator animator) {
             GameOverAction winAction = (GameOverAction) action;
-            Color teamcolor = animator.teams[winAction.getTeam()][0].getTeamColor();
-                MessageUiGameEndedAction gameEndedAction;
-                if(animator.gameMode==GameMode.Campaign||animator.gameMode==GameMode.Exam_Admission) {
+            MessageUiGameEndedAction gameEndedAction;
+            if (winAction.getTeam() < 0) {
+                gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, true);
+            } else {
+                Color teamcolor = animator.teams[winAction.getTeam()][0].getTeamColor();
+                if (animator.gameMode == GameMode.Campaign || animator.gameMode == GameMode.Exam_Admission) {
+
                     if (winAction.getTeam() == 0) {
                         //if the player 0 (human or bot of student) has not won then display defeat
-                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, true,winAction.getTeam(),teamcolor);
-                    } else if (winAction.getTeam()<0) {
-                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger,true);
+                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, true, winAction.getTeam(), teamcolor);
                     } else {
-                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, false, winAction.getTeam(),teamcolor);
+                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, false, winAction.getTeam(), teamcolor);
                     }
-                }
-                else {
-                    if (winAction.getTeam() < 0) {
-                        //gameEndedAction = new MessageUiGameEndedAction(0,animator.uiMessenger,true, winAction.getTeam());
-                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, true);
-                    } else {
+                } else {
+                    gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, true, winAction.getTeam(), teamcolor);
 
-                        gameEndedAction = new MessageUiGameEndedAction(0, animator.uiMessenger, true, winAction.getTeam(), animator.teams[winAction.getTeam()][0].getTeamColor());
-                    }
                 }
+            }
 
             return new ExpandedAction(gameEndedAction);
         }
