1e793dbe52d978f1ab517c84018fe6383c232852
Niggo <nicolas.kautt@st.ovgu.de>
2023-06-07 14:27:01 +0200

    #578 reformat

--- core/src/com/gats/ui/menu/buttons/BotSelectorTable.java
+++ core/src/com/gats/ui/menu/buttons/BotSelectorTable.java
@@ -15,6 +15,7 @@ public class BotSelectorTable extends LimitedWidthTable {
 	Manager.NamedPlayerClass[] availableBots;
 
 	int currentBotAmount;
+
 	public BotSelectorTable(Skin skin, int columnWidth) {
 		super(skin, columnWidth);
 		botSelectors = new ArrayList<>();
@@ -37,23 +38,24 @@ public class BotSelectorTable extends LimitedWidthTable {
 
 		Iterator<SelectBox<Manager.NamedPlayerClass>> currentBotSelection = botSelectors.iterator();
 
-		for (int i = 0; i < amountOfBots;i++) {
+		for (int i = 0; i < amountOfBots; i++) {
 			this.add(currentBotSelection.next());
 		}
 
-		currentBotAmount=amountOfBots;
+		currentBotAmount = amountOfBots;
 	}
 
 
 	/**
 	 * creates new Select Boxes and puts them into {@link BotSelectorTable#botSelectors}
+	 *
 	 * @param neededSelectBoxes Number of Select boxes to add to the list
 	 */
-	private void fillBotSelectorList(int neededSelectBoxes){
+	private void fillBotSelectorList(int neededSelectBoxes) {
 		//botSelectors is not shrinked, because if we increase
 		// the number of selections again, we remember previously selected values
 
-		for (int i =0;i<neededSelectBoxes;i++){
+		for (int i = 0; i < neededSelectBoxes; i++) {
 			SelectBox<Manager.NamedPlayerClass> newBox = new SelectBox<>(this.getSkin());
 			newBox.setItems(availableBots);
 			botSelectors.add(newBox);
@@ -62,13 +64,14 @@ public class BotSelectorTable extends LimitedWidthTable {
 
 	/**
 	 * Gets the selected Bots from {@link BotSelectorTable#botSelectors} and returns them inside a List.
+	 *
 	 * @return
 	 */
-	public ArrayList<Class<? extends Player>> evaluateSelected(){
+	public ArrayList<Class<? extends Player>> evaluateSelected() {
 		ArrayList<Class<? extends Player>> selectedBots = new ArrayList<>();
 		Iterator<SelectBox<Manager.NamedPlayerClass>> botSelectorsIterator = botSelectors.iterator();
 		//only add the number of bots corresponding to the currently selected ones
-		for(int i= 0;i<currentBotAmount;i++){
+		for (int i = 0; i < currentBotAmount; i++) {
 			selectedBots.add(botSelectorsIterator.next().getSelected().getClassRef());
 		}
 
@@ -77,29 +80,33 @@ public class BotSelectorTable extends LimitedWidthTable {
 
 	/**
 	 * Sets the List of bots that can be selected from the SelectBoxes
+	 *
 	 * @param bots
 	 */
-	public void setAvailableBots(Manager.NamedPlayerClass[] bots){
+	public void setAvailableBots(Manager.NamedPlayerClass[] bots) {
 		this.availableBots = bots;
-		for (SelectBox<Manager.NamedPlayerClass> box:botSelectors) {
+		for (SelectBox<Manager.NamedPlayerClass> box : botSelectors) {
 			box.setItems(availableBots);
 		}
 	}
 
 	/**
 	 * Tries to set the passed List as the selected values, if a value can not bo set, the default value stays set.
+	 *
 	 * @param players
 	 */
 	public void setSelected(ArrayList<Class<? extends Player>> players) {
 
-		if(players==null){return;}
+		if (players == null) {
+			return;
+		}
 		resizeTable(players.size());
 		Iterator<SelectBox<Manager.NamedPlayerClass>> select = botSelectors.iterator();
 		int availBotIndex;
-		for ( Class<? extends Player> bot:players) {
+		for (Class<? extends Player> bot : players) {
 			SelectBox<Manager.NamedPlayerClass> box = select.next();
-			availBotIndex = botAvailable(availableBots,bot);
-			if(availBotIndex>-1){
+			availBotIndex = botAvailable(availableBots, bot);
+			if (availBotIndex > -1) {
 				box.setSelected(availableBots[availBotIndex]);
 			}
 		}
@@ -107,14 +114,15 @@ public class BotSelectorTable extends LimitedWidthTable {
 
 	/**
 	 * Checks if a bot is available in the provided list
+	 *
 	 * @param bots
 	 * @param toSearch
 	 * @return
 	 */
-	private int botAvailable(Manager.NamedPlayerClass[] bots, Class<? extends Player> toSearch){
+	private int botAvailable(Manager.NamedPlayerClass[] bots, Class<? extends Player> toSearch) {
 		int index = 0;
-		for (Manager.NamedPlayerClass bot: bots) {
-			if (bot.getClassRef().equals(toSearch)){
+		for (Manager.NamedPlayerClass bot : bots) {
+			if (bot.getClassRef().equals(toSearch)) {
 				return index;
 			}
 			index++;
