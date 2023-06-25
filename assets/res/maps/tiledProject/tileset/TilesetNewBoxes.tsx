a671e8bfc74a4be46d022b59a2e53e2151b22b04
Niggo <nicolas.kautt@st.ovgu.de>
2023-06-25 16:22:32 +0200

    #598 fix tileset wich was old for some reason and fix mondlandschaft also changed version number

--- assets/res/maps/tiledProject/tileset/TilesetNewBoxes.tsx
+++ assets/res/maps/tiledProject/tileset/TilesetNewBoxes.tsx
@@ -1,61 +1,61 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="TilesetWithNewBoxes" tilewidth="16" tileheight="16" tilecount="10" columns="0" objectalignment="center" tilerendersize="grid" fillmode="preserve-aspect-fit">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="1">
   <properties>
    <property name="Beschreibung" value="Ankerbox"/>
   </properties>
   <image width="16" height="16" source="tilesetAssets/16x_anchor02.png"/>
  </tile>
- <tile id="1">
+ <tile id="2">
   <properties>
    <property name="Beschreibung" value="Normale Box"/>
   </properties>
   <image width="16" height="16" source="tilesetAssets/16x_box01DarkEdges.png"/>
  </tile>
- <tile id="2">
+ <tile id="3">
   <properties>
    <property name="Beschreibung" value="Schleimbox"/>
   </properties>
   <image width="16" height="16" source="tilesetAssets/slimeBox.png"/>
  </tile>
- <tile id="3">
+ <tile id="4">
   <properties>
    <property name="Beschreibung" value="Waffenbox-Normal"/>
   </properties>
   <image width="16" height="16" source="tilesetAssets/weaponBoxChest.png"/>
  </tile>
- <tile id="4">
+ <tile id="5">
   <properties>
    <property name="Beschreibung" value="Waffenbox-Anker"/>
   </properties>
   <image width="16" height="16" source="tilesetAssets/weaponBoxAnchor.png"/>
  </tile>
- <tile id="5">
+ <tile id="6">
   <properties>
    <property name="Beschreibung" value="Medibox"/>
   </properties>
   <image width="16" height="16" source="tilesetAssets/healthBoxRC.png"/>
  </tile>
-<tile id="100">
+<tile id="101">
  <properties>
   <property name="Beschreibung" value="Spawnpunkt Team 1"/>
  </properties>
  <image width="16" height="16" source="tilesetAssets/spawpointIndicator1.png"/>
 </tile>
-<tile id="101">
+<tile id="102">
  <properties>
   <property name="Beschreibung" value="Spawnpunkt Team 2"/>
  </properties>
  <image width="16" height="16" source="tilesetAssets/spawpointIndicator2.png"/>
 </tile>
-<tile id="102">
+<tile id="103">
  <properties>
   <property name="Beschreibung" value="Spawnpunkt Team 3"/>
  </properties>
  <image width="16" height="16" source="tilesetAssets/spawpointIndicator3.png"/>
 </tile>
-<tile id="103">
+<tile id="104">
  <properties>
   <property name="Beschreibung" value="Spawnpunkt Team 4"/>
  </properties>
